695a39966aaaa8a642b9e0f99c90e83d27cbfc47
zxf1023818103 <17204129@stu.nchu.edu.cn>
2020-05-13 21:24:53 +0800

    Initial commit

---
+++ src/test/java/webadv/example/P03ApplicationTests.java
@@ -0,0 +1,13 @@
+package webadv.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
